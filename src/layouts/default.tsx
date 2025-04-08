a98d153cc4e865c72b518a83f6733893592d73dc
Aaron <70713574+A-Singh15@users.noreply.github.com>
2025-04-08 12:50:48 -0700

    Update default.tsx

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { Navbar } from "@/components/navbar";
 import "@/styles/globals.css";
 import "../styles/globals.css"; // if you're one level down
@@ -8,26 +7,11 @@ export default function DefaultLayout({
                                       }: {
   children: React.ReactNode;
 }) {
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
 
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-      {/* 🌐 Non-interactive Iframe Background */}
-      <div className="fixed top-0 left-0 w-full h-screen -z-10">
-        {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
-        <iframe
-          ref={iframeRef}
-          src="https://codepen.io/A-Singh15/full/mydYePw"
-          frameBorder="0"
-          allowFullScreen
-          className="w-full h-full"
-          style={{
-            border: "none",
-            pointerEvents: "none", // disables interaction
-            zIndex: 0,
-          }}
-        />
-      </div>
+      {/* 🎥 MP4 Video Background */}
+
 
       {/* 🧭 Fixed Navbar */}
       <div className="fixed top-0 left-0 w-full z-20">
@@ -38,8 +22,6 @@ export default function DefaultLayout({
       <main className="pt-24 pb-20 px-6 container mx-auto max-w-7xl relative z-10">
         {children}
       </main>
-
-
     </div>
   );
 }
